dcdcd01219a8c0e7dcd996a2f4ecfb888eaf97a0
梅林的法杖 <530264127@qq.com>
2020-04-23 15:23:39 +0800

    feat: 🎸 wxx

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  dest: 'dist',
+  title: '开课吧前端',
+  themeConfig: {
+    editLinks: false,
+    docsDir: 'docs',
+    search: false,
+    lastUpdated: "更新时间",
+    nav: [],
+    sidebar: [
+      {
+        title: "vue",
+        path: "/vue/",
+        collapsable: true,
+        sidebarDepth: 1,
+        children: [
+        ]
+      },
+    ]
+  }
+}
